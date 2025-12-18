cfbe4aaebf327fcf85f705bda023f18808eb576b
Tran Loi <92966362+iwy2th@users.noreply.github.com>
2025-12-18 17:40:02 +0700

    Update JustJava.java with animal classes and main implementation

--- JustJava.java
+++ JustJava.java
@@ -2,5 +2,29 @@ public class Main {
   public static void main(String[] args) {
     String message = "Hello World, We are here !";
     System.out.println(message);
+
+    Animal dog = new Dog();
+    Animal cat = new Cat();
+
+    System.out.println(dog.speak());
+    System.out.println(cat.speak());
+  }
+}
+
+interface Animal {
+  String speak();
+}
+
+class Dog implements Animal {
+  @Override
+  public String speak() {
+    return "Woof! Woof!";
+  }
+}
+
+class Cat implements Animal {
+  @Override
+  public String speak() {
+    return "Meow! Meow!";
   }
 }
